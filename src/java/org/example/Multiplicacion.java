aba296597f01aa48dd6e233978a570f8c30be606
dilan.villada <dilanvm24@>
2025-08-12 11:39:41 -0500

    feat se actuliza

--- src/java/org/example/Multiplicacion.java
+++ src/java/org/example/Multiplicacion.java
@@ -3,7 +3,12 @@ package java.org.example;
 import java.util.Scanner;
 
 public class Multiplicacion {
-    public static void main(String[] args) {
+    public static Integer multiplicacion(Integer num1, Integer num2){
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("No se puede multiplicar si tenemos un numero es 0");
+
+        }
+        return num1 * num2;
 
     }
 }
